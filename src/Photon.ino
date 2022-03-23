aa36377032dfb72144a557bf8b424203316b5496
jeffreycaine <me@jeffwilliams.dev>
2022-03-23 10:51:17 -0600

    feat: status LEDs

--- src/Photon.ino
+++ src/Photon.ino
@@ -1,61 +1,68 @@
 /*
- * Project Report Activity
+ * Project: Accountability
  * Description:
  * Author: Jeffrey Williams
  * Date: March 11, 2022
  */
 
-#define myStatus D5
-#define myCircleStatus D6
-#define deviceStatus D7
-#define newReport D0
-#define help D1
-#define motivation D2
+#define myStatusGreen D0  // myStatus due
+#define myStatusBlue D1  // myStatus complete
+#define myStatusYellow D2  // myStatus lapsed
+#define circleStatusGreen D3
+#define circleStatusBlue RX
+#define circleStatusYellow TX
+#define connect D4
+#define help D5
+#define human D6  // publish human
+#define report D7  // publish report
 
-bool statusLED = LOW;  // onboard device LED
 int b0State;           // the current reading from the input pin
 int b0LastState = LOW; // the previous reading from the input pin
 int b1State;           // the current reading from the input pin
 int b1LastState = LOW; // the previous reading from the input pin
 int b2State;           // the current reading from the input pin
 int b2LastState = LOW; // the previous reading from the input pin
+int b3State;           // the current reading from the input pin
+int b3LastState = LOW; // the previous reading from the input pin
 
 unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
 unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
 
-int myStatusLED(String command);
+int myStatusCommand(String command);
+int circleStatusCommand(String command);
 
 // setup() runs once, when the device is first turned on.
 void setup()
 {
   // Put initialization like pinMode and begin functions here.
-  pinMode(newReport, INPUT_PULLDOWN);
+  pinMode(myStatusGreen, OUTPUT);
+  pinMode(myStatusBlue, OUTPUT);
+  pinMode(myStatusYellow, OUTPUT);
+  pinMode(circleStatusGreen, OUTPUT);
+  pinMode(circleStatusBlue, OUTPUT);
+  pinMode(circleStatusYellow, OUTPUT);
+  pinMode(report, INPUT_PULLDOWN);
+  pinMode(human, INPUT_PULLDOWN);
   pinMode(help, INPUT_PULLDOWN);
-  pinMode(motivation, INPUT_PULLDOWN);
-  pinMode(myStatus, OUTPUT);
-  pinMode(myCircleStatus, OUTPUT);
-  pinMode(deviceStatus, OUTPUT);
+  pinMode(connect, INPUT_PULLDOWN);
 
-  Particle.function("myStatus", myStatusLED);
+  Particle.function("myStatus", myStatusCommand);
+  Particle.function("circleStatus", circleStatusCommand);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop()
 {
-  // read the state of the switch into a local variable:
-  int b0Reading = digitalRead(newReport);
-
+  // report button
+  int b0Reading = digitalRead(report);
   // check to see if you just pressed the button
   // (i.e. the input went from LOW to HIGH), and you've waited long enough
   // since the last press to ignore any noise:
-
   // If the switch changed, due to noise or pressing:
   if (b0Reading != b0LastState)
   {
     // reset the debouncing timer
     lastDebounceTime = millis();
   }
-
   if ((millis() - lastDebounceTime) > debounceDelay)
   {
     // whatever the reading is at, it's been there for longer than the debounce
@@ -68,43 +75,31 @@ void loop()
       if (b0State == HIGH)
       {
         // publish event
-        bool success;
-        success = Particle.publish("Report");
+        bool success = Particle.publish("Report");
         if (!success)
         {
-          for (int i = 0; i < 6; i++)
-          {
-            statusLED = !statusLED;
-            digitalWrite(deviceStatus, statusLED);
-            delay(250);
-          }
+          // error handling
         }
       }
     }
   }
-
   // subscribe to status of active report conversation
   // ledState to be HIGH until conversation completed then to be LOW until next attempt to report activity
-  // set the LED:
-  // digitalWrite(ledPin, ledState);
 
-  // save the reading. Next time through the loop, it'll be the b0LastState:
+  // save the reading. Next time through the loop, it'll be the LastState:
   b0LastState = b0Reading;
 
-  // read the state of the switch into a local variable:
-  int b1Reading = digitalRead(help);
-
+  // human button
+  int b1Reading = digitalRead(human);
   // check to see if you just pressed the button
   // (i.e. the input went from LOW to HIGH), and you've waited long enough
   // since the last press to ignore any noise:
-
   // If the switch changed, due to noise or pressing:
   if (b1Reading != b1LastState)
   {
     // reset the debouncing timer
     lastDebounceTime = millis();
   }
-
   if ((millis() - lastDebounceTime) > debounceDelay)
   {
     // whatever the reading is at, it's been there for longer than the debounce
@@ -117,93 +112,47 @@ void loop()
       if (b1State == HIGH)
       {
         // publish event
-        bool success;
-        success = Particle.publish("help");
+        bool success = Particle.publish("Human");
         if (!success)
         {
-          for (int i = 0; i < 6; i++)
-          {
-            statusLED = !statusLED;
-            digitalWrite(deviceStatus, statusLED);
-            delay(250);
-          }
+          // error handling
         }
       }
     }
   }
+  // subscribe to status of active human conversation
+  // ledState to be HIGH until conversation completed then to be LOW until next attempt
 
-  // subscribe to status of active report conversation
-  // ledState to be HIGH until conversation completed then to be LOW until next attempt to report activity
-  // set the LED:
-  // digitalWrite(ledPin, ledState);
-
-  // save the reading. Next time through the loop, it'll be the b0LastState:
+  // save the reading. Next time through the loop, it'll be the LastState:
   b1LastState = b1Reading;
-
-  // read the state of the switch into a local variable:
-  int b2Reading = digitalRead(motivation);
-
-  // check to see if you just pressed the button
-  // (i.e. the input went from LOW to HIGH), and you've waited long enough
-  // since the last press to ignore any noise:
-
-  // If the switch changed, due to noise or pressing:
-  if (b2Reading != b2LastState)
-  {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay)
-  {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
-
-    // if the button state has changed:
-    if (b2Reading != b2State)
-    {
-      b2State = b2Reading;
-      if (b2State == HIGH)
-      {
-        // publish event
-        bool success;
-        success = Particle.publish("Motivation");
-        if (!success)
-        {
-          for (int i = 0; i < 6; i++)
-          {
-            statusLED = !statusLED;
-            digitalWrite(deviceStatus, statusLED);
-            delay(250);
-          }
-        }
-      }
-    }
-  }
-
-  // subscribe to status of active report conversation
-  // ledState to be HIGH until conversation completed then to be LOW until next attempt to report activity
-  // set the LED:
-  // digitalWrite(ledPin, ledState);
-
-  // save the reading. Next time through the loop, it'll be the b0LastState:
-  b2LastState = b2Reading;
-
-  digitalWrite(deviceStatus, LOW);
-}
+} // end loop()
 
 // this function automagically gets called upon a matching POST request
-int myStatusLED(String command)
+int myStatusCommand(String command)
 {
-  // look for the matching argument "coffee" <-- max of 64 characters long
   if (command == "complete")
   {
-    digitalWrite(myStatus, HIGH);
+    analogWrite(myStatusBlue, 255);
     return 1;
   }
-  else if (command == "reset")
+  else if (command == "due")
   {
-    digitalWrite(myStatus, LOW);
+    analogWrite(myStatusGreen, 255);
+    return 1;
+  }
+  else if (command == "lapsed")
+  {
+    analogWrite(myStatusYellow, 255);
+    return 1;
+  }
+  else if (command == "reporting")
+  {
+    analogWrite(myStatusBlue, 125);
+    return 1;
+  }
+  else if (command == "dropped")
+  {
+    analogWrite(myStatusYellow, 125);
     return 1;
   }
   else
